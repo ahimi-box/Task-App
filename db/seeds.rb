594febb25237865e63d3b3e3b7a1e8963c59021a
ahimi <testtestabe07@gmail.com>
2021-03-18 01:58:50 +0000

    Finish User-page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@ User.create!( name: "Sample User",
               password: "password",
               password_confirmation: "password",
               admin: true)
+
+100.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+@users = User.order(:created_at).take(3)
+50.times do
+  name  = Faker::Name.name
+  description = Faker::Lorem.sentence(5)
+  @users.each { |user| user.tasks.create!(name: name,description: description) }
+end
